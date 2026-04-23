4c3cafd52cc46e3f1a8f2f94ec5b9f0825484c85
曼岛TT <2116925408@qq.com>
2026-04-23 22:32:42 +0800

    RBTree

---
+++ RBTree/RBTree.h
@@ -0,0 +1,275 @@
+#pragma once
+enum Colour
+{
+	RED,
+	BLACK
+};
+// 这里我们默认按key/value结构实现
+template<class T>
+struct RBTreeNode
+{
+	// 这里更新控制平衡也要加入parent指针
+	T _data;
+	RBTreeNode<T>* _left;
+	RBTreeNode<T>* _right;
+	RBTreeNode<T>* _parent;
+	Colour _col;
+	RBTreeNode(const T& data)
+		:_data(data)
+		, _left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+	{
+	}
+};
+template<class K, class T,class KeyofT>
+class RBTree
+{
+	typedef RBTreeNode<T> Node;
+public:
+	bool Insert(const T& data)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(data);
+			_root->_col = BLACK;
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_data<data)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		cur->_col = RED;
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		// 链接父亲
+		cur->_parent = parent;
+		//父亲是红色的，出现连续的红色节点，需要处理
+		while (parent&&parent->_col == RED)
+		{
+			Node* grandfather = parent->_parent;
+			//   g
+			// p    u
+			if (parent==grandfather->_left)
+			{
+				Node* uncle = grandfather->_right;
+				if (uncle && uncle->_col == RED)
+				{
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+					//继续向上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else
+				{
+					//      g
+					//   p     u
+					// c
+					if (cur == parent->_left)
+					{
+						RotateR(grandfather);
+						parent->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else
+					{
+						RotateL(parent);
+						RotateR(grandfather);
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					break;
+				}
+			}
+			//    g
+			// u     p
+			else
+			{
+				Node* uncle = grandfather->_left;
+				// 叔叔存在且为红，-》变色即可
+				if (uncle && uncle->_col == RED)
+				{
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+					// 继续往上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else // 叔叔不存在，或者存在且为黑
+				{
+					// 情况二：叔叔不存在或者存在且为黑
+					// 旋转+变色
+					// g
+					// u p
+					// c
+					if (cur == parent->_right)
+					{
+						RotateL(grandfather);
+						parent->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else
+					{
+						// g
+						// u p
+						// c
+						RotateR(parent);
+						RotateL(grandfather);
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					break;
+				}
+			}
+		}
+		_root->_col = BLACK;
+		return true;
+	}
+	void RotateR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+
+		parent->_left = subLR;
+		if (subLR)
+			subLR->_parent = parent;
+
+		Node* pParent = parent->_parent;
+
+		subL->_right = parent;
+		parent->_parent = subL;
+
+		if (parent == _root)
+		{
+			_root = subL;
+			subL->_parent = nullptr;
+		}
+		else
+		{
+			if (pParent->_left == parent)
+			{
+				pParent->_left = subL;
+			}
+			else
+			{
+				pParent->_right = subL;
+			}
+
+			subL->_parent = pParent;
+		}
+	}
+
+	void RotateL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		parent->_right = subRL;
+		if (subRL)
+			subRL->_parent = parent;
+
+		Node* parentParent = parent->_parent;
+		subR->_left = parent;
+		parent->_parent = subR;
+		if (parentParent == nullptr)
+		{
+			_root = subR;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			if (parent == parentParent->_left)
+			{
+				parentParent->_left = subR;
+			}
+			else
+			{
+				parentParent->_right = subR;
+			}
+			subR->_parent = parentParent;
+		}
+	}
+
+	
+
+	int Height()
+	{
+		return _Height(_root);
+	}
+
+	int Size()
+	{
+		return _Size(_root);
+	}
+
+	Node* Find(const K& key)
+	{
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < key)
+			{
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > key)
+			{
+				cur = cur->_left;
+			}
+			else
+			{
+				return cur;
+			}
+		}
+
+		return nullptr;
+	}
+
+	
+
+private:
+
+
+	int _Height(Node* root)
+	{
+		if (root == nullptr)
+			return 0;
+		int leftHeight = _Height(root->_left);
+		int rightHeight = _Height(root->_right);
+		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+	}
+
+	int _Size(Node* root)
+	{
+		if (root == nullptr)
+			return 0;
+
+		return _Size(root->_left) + _Size(root->_right) + 1;
+	}
+
+private:
+	Node* _root = nullptr;
+};
