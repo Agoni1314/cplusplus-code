5cdb49fe2055b65cda78effeb13b573cd27ff30d
曼岛TT <2116925408@qq.com>
2026-05-13 23:32:15 +0800

    t

---
+++ template/template/template_p.cpp
@@ -0,0 +1,9 @@
+#include"template_pro.h"
+//template<class T>
+//void func(const int& left, const int& right)
+//{
+//	cout <<"void func(conat int& left, conat int& right)"<< endl;
+//}
+//显示实例化
+//template
+//int Add(const int& left, const int& right)
